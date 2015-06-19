355fed714b5f13e452aaf3403ce3857873092d88
Mitchell Gu <mitchgu@mitchgu.com>
2015-06-19 15:13:11 -0400

    Added bridge example

--- toolpathing.py
+++ toolpathing.py
@@ -37,6 +37,5 @@ class Toolpath:
 
   def export_toolpath(self):
     with open('toolpath.txt', 'wb') as myfile:
-      wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-      for instruction in self.toolpath:
-        wr.writerow(instruction)
+      wr = csv.writer(myfile), quoting=csv.QUOTE_ALL)
+      wr.writerows(self.vertices)
